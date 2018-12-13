ecb2b253670f57e6a26153658b8a6c902812f060
Kaloyan Kostov <kaloyanit@yahoo.com>
2018-12-14 00:39:39 +0200

    Add entitiframework core setup

--- Blog/Blog/Startup.cs
+++ Blog/Blog/Startup.cs
@@ -1,20 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Blog.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Blog
 {
     public class Startup
     {
+        private readonly IHostingEnvironment environment;
+        private readonly IConfiguration configuration;
+
+        public IHostingEnvironment Environment => this.environment;
+
+        public IConfiguration Configuration => this.configuration;
+
+        public Startup(IHostingEnvironment environment, IConfiguration configuration)
+        {
+            this.environment = environment;
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BlogContext>(options => options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));             
+
+
+            services.AddMvc();
+            services.AddRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +39,17 @@ namespace Blog
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes => {
+
             });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
