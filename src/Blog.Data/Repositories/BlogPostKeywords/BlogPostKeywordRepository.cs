662e0291c15a15076680803182a79186a3524046
Kaloyan Kostov <kaloyanit@yahoo.com>
2019-07-01 23:19:01 +0300

    Add repositories for all models and register in startup

---
+++ src/Blog.Data/Repositories/BlogPostKeywords/BlogPostKeywordRepository.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Data.Models;
+using Blog.DataAccess.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Data.Repositories.BlogPostKeywords
+{
+    public class BlogPostKeywordRepository : SqlServerEntityFrameworkCrudRepository<BlogPostKeyword, BlogContext>, IBlogPostKeywordRepository
+    {
+        public BlogPostKeywordRepository(BlogContext context) : base(context)
+        {
+        }
+
+        protected override DbSet<BlogPostKeyword> EntityDbSet => this.Context.BlogPostKeywords;
+    }
+}
---
+++ src/Blog.Data/Repositories/Tags/TagRepository.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Data.Models;
+using Blog.DataAccess.SqlServer;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data.Repositories.Tags
+{
+    public class TagRepository : SqlServerEntityFrameworkCrudRepository<Tag, BlogContext>, ITagSqlRepository
+    {
+        public TagRepository(BlogContext context) : base(context)
+        {
+        }
+
+        protected override DbSet<Tag> EntityDbSet => this.Context.Tags;
+    }
+}
