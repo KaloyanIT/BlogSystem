0ed437f163326615aac4b74c0862031ff5eaf273
Kaloyan Kostov <kaloyanit@yahoo.com>
2019-07-01 23:07:51 +0300

    Add constructors to domain models

--- src/Blog.Data/Models/BlogPostTag.cs
+++ src/Blog.Data/Models/BlogPostTag.cs
@@ -12,5 +12,39 @@ namespace Blog.Data.Models
         public Guid TagId { get; set; }
 
         public virtual Tag Tag { get; set; }
+
+        public BlogPostTag() { }
+
+        public BlogPostTag(Guid blogPostId, Guid tagId)
+        {
+            if (blogPostId == Guid.Empty)
+            {
+                throw new ArgumentNullException("BlogPostTag blogPostId can not be empty guid.");
+            }
+
+            if (tagId == Guid.Empty)
+            {
+                throw new ArgumentNullException("BlogPostTag tagId can not be empty guid.");
+            }
+
+            this.BlogPostId = blogPostId;
+            this.TagId = tagId;
+        }
+
+        public BlogPostTag(BlogPost blogPost, Tag tag)
+        {
+            if(blogPost == null)
+            {
+                throw new ArgumentNullException("Blog post can not be null.");
+            }
+
+            if(tag == null)
+            {
+                throw new ArgumentNullException("Tag can not be null.");
+            }
+
+            this.BlogPost = blogPost;
+            this.Tag = tag;
+        }
     }
 }
