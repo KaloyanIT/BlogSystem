c0c159c996fc1c64e1d7ec2c5afefce00a033289
KaloyanIT <kaloyan.kostov@yahoo.com>
2019-07-04 17:52:22 +0300

    Unit tests for: - Comment - Keyword - Tag - BaseDbObject

--- src/Blog.Data/Models/Comment.cs
+++ src/Blog.Data/Models/Comment.cs
@@ -22,36 +22,37 @@ namespace Blog.Data.Models
 
         public Comment() { }
 
-        public Comment(Guid itemId, string userId, string username, string email, string content)
+        public Comment(Guid itemId, string username, string email, string content, string userId = "")
         {
-            if(itemId == null || itemId == Guid.Empty)
+            if (itemId == null || itemId == Guid.Empty)
             {
                 throw new ArgumentNullException("Comment blogId can not be null or empty Guid.");
             }
 
-            if(string.IsNullOrWhiteSpace(username))
+            if (string.IsNullOrWhiteSpace(username))
             {
                 throw new ArgumentNullException("Comment username can not be null or empty string.");
             }
 
-            if(string.IsNullOrWhiteSpace(email))
+            if (string.IsNullOrWhiteSpace(email))
             {
                 throw new ArgumentNullException("Comment email can not be null or empty string.");
             }
 
-            if(string.IsNullOrWhiteSpace(userId))
-            {
-                throw new ArgumentNullException("UserId can not be null or empty string.");
-            }
 
-            if(string.IsNullOrWhiteSpace(content))
+            if (string.IsNullOrWhiteSpace(content))
             {
                 throw new ArgumentNullException("Comment content can not be null or empty string");
             }
 
+            if (!string.IsNullOrWhiteSpace(userId))
+            {
+                this.UserId = userId;
+
+                //throw new ArgumentNullException("UserId can not be null or empty string.");
+            }
             this.AttachedItemId = itemId;
             this.Username = username;
-            this.UserId = userId;
             this.Email = email;
             this.Content = content;
         }
---
+++ test/Blog.UnitTests/Domain/CommentShould.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Blog.Data.Models;
+using Xunit;
+
+namespace Blog.UnitTests.Domain
+{
+    public class CommentShould
+    {
+        public string validString = "validWord";
+
+        [Fact]
+        public void ThrowArgumentNullException_WhenItemId_IsGuidEmpty()
+        {
+            //Arrange
+            //Act
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => new Comment(Guid.Empty, this.validString, this.validString, this.validString));
+        }
+
+        [Fact]
+        public void SetItemIdProperty_WhenIsValid()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+            //Act
+            var comment = new Comment(id, this.validString, this.validString, this.validString);
+
+            //Assert
+            Assert.Equal(id, comment.AttachedItemId);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void ThrowArgumentNullException_WhenUsername_IsNUll(string invalidString)
+        {
+            //Arrange
+            //Act
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => new Comment(Guid.NewGuid(), invalidString, this.validString, this.validString));
+        }
+
+        [Fact]
+        public void SetUsernameProperty_WhenIsValid()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+            string username = "Username";
+            //Act
+            var comment = new Comment(id, username, this.validString, this.validString);
+
+            //Assert
+            Assert.Equal(username, comment.Username);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void ThrowArgumentNullException_WhenEmail_IsNUll(string invalidString)
+        {
+            //Arrange
+            //Act
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => new Comment(Guid.NewGuid(), this.validString, invalidString, this.validString));
+        }
+
+        [Fact]
+        public void SetEmailProperty_WhenIsValid()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+            string username = "Username";
+            string email = "email";
+            //Act
+            var comment = new Comment(id, username, email, this.validString);
+
+            //Assert
+            Assert.Equal(email, comment.Email);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void ThrowArgumentNullException_WhenContent_IsNUll(string invalidString)
+        {
+            //Arrange
+            //Act
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => new Comment(Guid.NewGuid(), this.validString, this.validString, invalidString));
+        }
+
+        [Fact]
+        public void SetContentProperty_WhenIsValid()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+            string username = "Username";
+            string email = "email";
+            string content = "content";
+
+            //Act
+            var comment = new Comment(id, username, email, content);
+
+            //Assert
+            Assert.Equal(content, comment.Content);
+        }
+
+        [Fact]
+        public void SetUserIdProperty_WhenIsNotNullOrWhitespace()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+            string username = "Username";
+            string email = "email";
+            string userId = "user-id";
+
+            //Act
+            var comment = new Comment(id, username, email, this.validString, userId);
+
+            //Assert
+            Assert.Equal(userId, comment.UserId);
+        }
+    }
+}
