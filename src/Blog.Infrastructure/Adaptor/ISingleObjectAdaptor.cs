ef419ae070d6c5f15cb2fc2838c12e7d45c84909
KaloyanIT <kaloyan.kostov@yahoo.com>
2019-07-17 00:10:00 +0300

    Add adaptor and validator strategies

---
+++ src/Blog.Infrastructure/Adaptor/ISingleObjectAdaptor.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.Infrastructure.Adaptor
+{
+    public interface ISingleObjectAdaptor<T, T1>
+        where T : class
+        where T1 : class
+    {
+        void Adapt(T fromValue, T1 toValue);
+    }
+}
