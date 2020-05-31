57e30fa61ba206c5bcbb2e1bd55b32ce02213b85
Kaloyan Kostov <kaloyan.kostov@yahoo.com>
2020-05-31 18:10:10 +0200

    Add fonts

--- src/Blog.Web/StaticFiles/src/index.js
+++ src/Blog.Web/StaticFiles/src/index.js
@@ -1,7 +1,1 @@
-﻿import _ from 'lodash';
-function createChild() {
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
-    return element;
-}
-document.body.appendChild(createChild());
+﻿
