619ef0eff22668687f7ef21393af1935c100c802
Kaloyan Kostov <kaloyan.kostov@yahoo.com>
2020-05-31 13:15:31 +0200

    Add TinyMCE and webpack libraries Add new Static Fiels FOleder loacation Add webpack configuration

---
+++ src/Blog.Web/webpack.config.js
@@ -0,0 +1,47 @@
+﻿const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const bundleFileName = 'bundle';
+const dirName = 'StaticFiles/dist';
+
+module.exports = (env, argv) => {
+    return {
+        mode: argv.mode === "production" ? "production" : "development",
+        entry: ['./StaticFiles/src/index.js', './StaticFiles/src/sass/index.scss'],
+        output: {
+            filename: bundleFileName + '.js',
+            path: path.resolve(__dirname, dirName)
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.s[c|a]ss$/,
+                    use:
+                        [
+                            'style-loader',
+                            MiniCssExtractPlugin.loader,
+                            'css-loader',
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    config: {
+                                        ctx: {
+                                            env: argv.mode
+                                        }
+                                    }
+                                }
+                            },
+                            'sass-loader'
+                        ]
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: bundleFileName + '.css'
+            })
+        ]
+    };
+};
