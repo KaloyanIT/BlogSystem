cf4bade4e02767162191e592356dff66f95e8c24
Kaloyan Kostov <kaloyanit@yahoo.com>
2020-06-02 00:14:50 +0200

    Add TinyMCE as internal Text Editor (#33)
    * Add TinyMCE and webpack libraries
    Add new Static Fiels FOleder loacation
    Add webpack configuration
    
    * Add Babel to Webpack
    
    * Add eslint rules
    
    * Add prettier to javascripts configuration
    
    * Add fonts
    
    * Add jquery support
    
    * Add production options for webpack
    
    * Add sections for scripts and styles in layouts
    
    * Add Tinymce as a HTML Editor
    
    * Add Tinymce to Blog Edit
    
    * Add TinyMCE and webpack libraries
    Add new Static Fiels FOleder loacation
    Add webpack configuration
    
    * Add Babel to Webpack
    
    * Add eslint rules
    
    * Add prettier to javascripts configuration
    
    * Add fonts
    
    * Update packages json wrong settings

--- src/Blog.Web/StaticFiles/src/js/text-editor.js
+++ src/Blog.Web/StaticFiles/src/js/text-editor.js
@@ -1,1 +1,34 @@
-﻿
+﻿/* eslint-disable no-undef */
+// eslint-disable-next-line func-names
+const textEditor = (function () {
+  function init(cssSelector) {
+    tinymce.init({
+      selector: cssSelector,
+      width: 600,
+      height: 300,
+      plugins: [
+        'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
+        'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+        'table emoticons template paste help',
+      ],
+      toolbar:
+        'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | ' +
+        'bullist numlist outdent indent | link image | print preview media fullpage | ' +
+        'forecolor backcolor emoticons | help',
+      menu: {
+        favs: {
+          title: 'My Favorites',
+          items:
+            'code visualaid | searchreplace | spellchecker | emoticons',
+        },
+      },
+      menubar: 'favs file edit view insert format tools table help',
+    });
+  }
+
+  return {
+    init,
+  };
+})();
+
+export { textEditor };
