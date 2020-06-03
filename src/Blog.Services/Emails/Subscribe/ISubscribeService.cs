0d2da946d284eefa8879c292e57419bab6d574b0
Kaloyan Kostov <kaloyan.kostov@yahoo.com>
2020-06-03 22:31:16 +0200

    Add Service for Emails

---
+++ src/Blog.Services/Emails/Subscribe/ISubscribeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Blog.Services.Base;
+
+namespace Blog.Services.Emails.Subscribe
+{
+    interface ISubscribeService : IService
+    {
+    }
+}
