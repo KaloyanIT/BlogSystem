ff58b168ee9fcff6c31bfdbef4f267e4945c262e
Kaloyan Kostov <kaloyan.kostov@yahoo.com>
2020-06-07 01:05:32 +0200

    Add Pager abstraction

---
+++ src/Blog.Infrastructure/Pager/PagedResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Blog.Infrastructure.Pager
+{
+    public class PagedResult<T> : PagedResultBase where T : class
+    {
+        public IList<T> Results { get; set; }       
+
+        public PagedResult()
+        {
+            Results = new List<T>();
+        }
+    }
+}
