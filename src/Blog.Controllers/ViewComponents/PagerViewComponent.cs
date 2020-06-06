fc7a878322c0f11af18b46d2ce1abe1839e99eb0
Kaloyan Kostov <kaloyan.kostov@yahoo.com>
2020-06-07 01:05:32 +0200

    Add Pager abstraction

---
+++ src/Blog.Controllers/ViewComponents/PagerViewComponent.cs
@@ -0,0 +1,14 @@
+﻿namespace Blog.Controllers.ViewComponents
+{
+    using System.Threading.Tasks;
+    using Blog.Infrastructure.Pager;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PagerViewComponent : ViewComponent
+    {       
+        public Task<IViewComponentResult> InvokeAsync(PagedResultBase result)
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", result));
+        }
+    }
+}
