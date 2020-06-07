31fbe68dee70672a25abe8ec692f5f04f7ce73ce
Kaloyan Kostov <kaloyan.kostov@yahoo.com>
2020-06-07 02:29:38 +0200

    Add basics for create a many to many connection

---
+++ src/Blog.Controllers/ViewModels/Admin/Emails/MailLists/MailListViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Blog.Controllers.ViewModels.Admin.Emails.MailLists
+{
+    using Blog.Data.Models.Emails;
+    using Blog.Infrastructure.AutoMapper;
+
+    public class MailListViewModel : IHaveMapFrom<MailList>
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
