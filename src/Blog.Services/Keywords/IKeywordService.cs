18d8ec233f1a8425e53ac7a9822588352e09a0f1
Kaloyan Kostov <kaloyan.kostov@yahoo.com>
2020-06-17 23:35:59 +0200

    Add Keywords grid

---
+++ src/Blog.Services/Keywords/IKeywordService.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.Services.Keywords
+{
+    using System.Linq;
+    using Base;
+    using Data.Models;
+
+    public interface IKeywordService : IService
+    {
+        IQueryable<Keyword> GetAll();
+    }
+}
---
+++ src/Blog.Services/Keywords/KeywordService.cs
@@ -0,0 +1,24 @@
+﻿namespace Blog.Services.Keywords
+{
+    using System.Linq;
+    using Data.Models;
+    using Data.Models.Context;
+
+    public class KeywordService : IKeywordService
+    {
+        private readonly BlogContext _blogContext;
+
+        public KeywordService(BlogContext blogContext)
+        {
+            _blogContext = blogContext;
+        }
+
+
+        public IQueryable<Keyword> GetAll()
+        {
+            var items = _blogContext.Keywords;
+
+            return items;
+        }
+    }
+}
