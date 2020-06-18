31850f7c6e75ba0b470cf9915ceb21e41b423084
Kaloyan Kostov <kaloyan.kostov@yahoo.com>
2020-06-18 23:03:56 +0200

    Add CRUD for Keywords object

--- src/Blog.Services/Keywords/IKeywordService.cs
+++ src/Blog.Services/Keywords/IKeywordService.cs
@@ -1,11 +1,22 @@
 ﻿namespace Blog.Services.Keywords
 {
+    using System;
     using System.Linq;
+    using System.Threading.Tasks;
     using Base;
     using Data.Models;
+    using Models;
 
     public interface IKeywordService : IService
     {
+        Task Create(CreateKeywordServiceModel serviceModel);
+
+        Task Edit(EditKeywordServiceModel serviceModel);
+
+        Task Delete(Guid id);
+
         IQueryable<Keyword> GetAll();
+
+        Task<Keyword> GetById(Guid id);
     }
 }
