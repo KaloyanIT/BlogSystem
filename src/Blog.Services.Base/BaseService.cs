209995b3afe068c67462b6648310512857793940
Kaloyan Kostov <kaloyan.kostov@yahoo.com>
2020-07-05 11:08:06 +0200

    Create base service type

---
+++ src/Blog.Services.Base/BaseService.cs
@@ -0,0 +1,18 @@
+﻿namespace Blog.Services.Base
+{
+    using System;
+    using AutoMapper;
+    using Microsoft.Extensions.Logging;
+
+    public class BaseService
+    {
+        protected IMapper Mapper { get; }
+        public ILogger<BaseService> Logger { get; }
+
+        public BaseService(IMapper mapper, ILogger<BaseService> logger)
+        {
+            Mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+    }
+}
