c19b92190f67fe9f7bfcc08cfb25ffbaea46e82d
Kaloyan Kostov <kaloyan.kostov@yahoo.com>
2020-07-18 09:09:05 +0200

    Add Service for OpenGraph

--- src/Blog.Data.Repositories/Meta/OpenGraphs/IOpenGraphRepository.cs
+++ src/Blog.Data.Repositories/Meta/OpenGraphs/IOpenGraphRepository.cs
@@ -3,8 +3,11 @@
     using Base;
     using Models.Meta;
     using DataAccess;
+    using System;
+    using System.Threading.Tasks;
 
     public interface IOpenGraphRepository : IRepository<OpenGraph>, ITransientRepository
     {
+        Task<OpenGraph> GetByAttachedItemId(Guid attachedItemId);
     }
 }
---
+++ src/Blog.Services/Meta/OpenGraphs/IOpenGraphService.cs
@@ -0,0 +1,15 @@
+﻿namespace Blog.Services.Meta.OpenGraphs
+{
+    using System.Linq;
+    using Data.Models.Meta;
+    using Base;
+    using System.Threading.Tasks;
+    using System;
+
+    public interface IOpenGraphService : IService
+    {
+        IQueryable<OpenGraph> GetAll();
+
+        Task<OpenGraph> GetByAttachedItemId(Guid itemId);
+    }
+}
