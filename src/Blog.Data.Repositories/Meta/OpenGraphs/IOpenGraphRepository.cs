995b68047012abada1e2e685b67e8687fba03c1c
Kaloyan Kostov <kaloyan.kostov@yahoo.com>
2020-07-18 08:57:53 +0200

    Add repositories

---
+++ src/Blog.Data.Repositories/Meta/OpenGraphs/IOpenGraphRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog.Data.Repositories.Meta.OpenGraphs
+{
+    using Base;
+    using Models.Meta;
+    using DataAccess;
+
+    public interface IOpenGraphRepository : IRepository<OpenGraph>, ITransientRepository
+    {
+    }
+}
---
+++ src/Blog.Data.Repositories/Meta/OpenGraphs/OpenGraphRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Blog.Data.Repositories.Meta.OpenGraphs
+{
+    using DataAccess.SqlServer;
+    using Microsoft.EntityFrameworkCore;
+    using Models.Context;
+    using Models.Meta;
+
+    public class OpenGraphRepository : SqlServerEntityFrameworkCrudRepository<OpenGraph, BlogContext>, IOpenGraphRepository
+    {
+        public OpenGraphRepository(BlogContext context) : base(context)
+        {
+        }
+
+        protected override DbSet<OpenGraph> EntityDbSet => EntityDbSet;
+    }
+}
