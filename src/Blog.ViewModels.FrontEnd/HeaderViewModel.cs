ed8b9d018ef6bde8d04e581c2491c1e8c3c42f0a
Kaloyan Kostov <kaloyan.kostov@yahoo.com>
2021-02-06 00:50:45 +0100

    Update packages Add header as ViewCompnent and change Title and Subtitle Dynamically

---
+++ src/Blog.ViewModels.FrontEnd/HeaderViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Blog.ViewModels.FrontEnd
+{
+    using System;
+
+    public class HeaderViewModel
+    {
+        public string Title { get; set; } = null!;
+
+        public string SubTitle { get; set; } = null!;
+
+        public DateTime? CreationDate { get; set; }
+
+        public string? Author { get; set; }
+    }
+}
